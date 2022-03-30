ba5b19869a35ed1ffe6d59a49958be34e494fef0
Elase Huang <elase.huang@outlook.com>
2022-03-30 17:26:54 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    application
+}
+
+group = "com.github.elase"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    // https://mvnrepository.com/artifact/com.alibaba/easyexcel
+    implementation("com.alibaba:easyexcel:3.0.5")
+    implementation(kotlin("reflect"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
